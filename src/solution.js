245dc5a3af14e5d2b175d83efb914046f73f895b
Julian Ng <julesnuggy@gmail.com>
2023-09-21 16:56:29 +0100

    Refactor: extract function to find number location

--- src/solution.js
+++ src/solution.js
@@ -6,6 +6,22 @@ const keypad = [
   [und, '0', und], // numRow 3
 ];
 
+const findNumberLocation = (number) => {
+  let numRowIndex, valueIndex;
+
+  // b. find the subarray index it belongs to...
+  keypad.forEach((numRow, numRowIdx) => {
+    if(numRow.includes(number)) {
+      // c. and track the index of the numRow
+      numRowIndex = numRowIdx;
+      // d. and find the index of the number in the numRow
+      valueIndex = numRow.indexOf(number);
+    }
+  });
+
+  return [numRowIndex, valueIndex];
+}
+
 const getPINs = (observed) => {
   // Convert string argument to array of strings
   // e.g. '123' --> ['1', '2', '3']
@@ -15,35 +31,25 @@ const getPINs = (observed) => {
   // 1. Find same-index values of adjacent numRows:
   // a. For each number...
   numbers.forEach(number => {
-    let currentRow, currentIndex;
     const adjacentNums = [];
-
-    // b. find the subarray index it belongs to...
-    keypad.forEach((numRow, numRowIdx) => {
-      if(numRow.includes(number)) {
-        // c. and track the index of the numRow
-        currentRow = numRowIdx;
-        // d. and find the index of the number in the numRow
-        currentIndex = numRow.indexOf(number);
-      }
-    });
+    const [numRowIndex, valueIndex] = findNumberLocation(number);
 
     // e. Get the number of the previous numRow with the same index
-    keypad[currentRow - 1] // Check prev numRow exists
-      && keypad[currentRow - 1][currentIndex] // Check value is not undefined
-      && adjacentNums.push(keypad[currentRow - 1][currentIndex]);
+    keypad[numRowIndex - 1] // Check prev numRow exists
+      && keypad[numRowIndex - 1][valueIndex] // Check value is not undefined
+      && adjacentNums.push(keypad[numRowIndex - 1][valueIndex]);
 
     // f. Get the number of the next numRow with the same index
-    keypad[currentRow + 1] // Check next numRow exists
-      && keypad[currentRow + 1][currentIndex] // Check value is not undefined
-      && adjacentNums.push(keypad[currentRow + 1][currentIndex])
+    keypad[numRowIndex + 1] // Check next numRow exists
+      && keypad[numRowIndex + 1][valueIndex] // Check value is not undefined
+      && adjacentNums.push(keypad[numRowIndex + 1][valueIndex])
 
     // 2. Find the next index number in the same numRow
-    keypad[currentRow][currentIndex + 1] // Check value is not undefined
-      && adjacentNums.push(keypad[currentRow][currentIndex + 1])
+    keypad[numRowIndex][valueIndex + 1] // Check value is not undefined
+      && adjacentNums.push(keypad[numRowIndex][valueIndex + 1])
 
     // 3. Find the previous index number in the same numRow
-    keypad[currentRow][currentIndex - 1] // Check value is not undefined
-      && adjacentNums.push(keypad[currentRow][currentIndex - 1])
+    keypad[numRowIndex][valueIndex - 1] // Check value is not undefined
+      && adjacentNums.push(keypad[numRowIndex][valueIndex - 1])
   });
 }
